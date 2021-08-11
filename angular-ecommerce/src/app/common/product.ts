fb5a32ac2e330092aa12ff9b5375d4dae52a139a
baitul-hossain <baitul.hossain@gmail.com>
2021-08-12 00:10:00 +0100

    angular front end

---
+++ angular-ecommerce/src/app/common/product.ts
@@ -0,0 +1,28 @@
+export class Product {
+
+    // constructor( sku:String, name:String, description:String, unitPrice:number,
+    //     imageUrl:String, active:boolean, unitsInStock:number, dateCreated:Date, lastUpdated:Date  ){
+    //     this.sku = sku, this.name = name, this.description = description, this.unitPrice = unitPrice,
+    //     this.imageUrl = imageUrl, this.active = active, this.unitsInStock = unitsInStock,
+    //     this.dateCreated = dateCreated, this.lastUpdated = lastUpdated
+    // }
+
+    sku: String;
+
+    name: String;
+
+    description: String;
+
+    unitPrice: number;
+
+    imageUrl: String;
+
+    active: boolean;
+
+    unitsInStock: number;
+
+    dateCreated: Date;
+
+    lastUpdated: Date;
+
+}
